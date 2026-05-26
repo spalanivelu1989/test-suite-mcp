7fbaf9314f48fa02a6a8445a6d9e807cbb592109
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2026-05-26 22:43:02 +0530

    fixes

--- dist/tools/list-runs.js
+++
@@ -1,8 +0,0 @@
-import { readdirSync, existsSync } from "fs";
-import { appRunsDir } from "../lib/paths.js";
-export function listRuns(app) {
-    const runs = appRunsDir(app);
-    if (!existsSync(runs))
-        return [];
-    return readdirSync(runs).sort().reverse();
-}
